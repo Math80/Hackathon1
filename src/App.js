edf103c542dcd1c30f3120d82a5ff94f60b5a92f
Julie Charlemagne <Julie.Charlemagne@live.fr>
2019-04-18 20:47:31 +0200

    Random eggs + golden

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Eggs from './Eggs/Eggs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Eggs/>
       </div>
     );
   }
---
+++ src/Eggs/Eggs.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import Egg from './Egg';
+import GoldenEgg from './GoldenEgg';
+import './Eggs.css';
+
+
+const eggsRandom = Array.from({length: 20}).map(egg => 
+  { 
+  return {
+  minShow: Math.floor(Math.random() * (10000 - 1)) + 1,
+  maxShow: Math.floor(Math.random() * (120000 - 35000)) + 35000,
+  minHide: Math.floor(Math.random() * (750 - 500)) + 500,
+  maxHide: Math.floor(Math.random() * (1250 - 1000)) + 1000,
+  coordX: Math.floor(Math.random() * (1500 - 50)) + 50,
+  coordY: Math.floor(Math.random() * (700 - 50)) + 50,
+  img: `images/egg${Math.floor(Math.random() * (9 - 1)) + 1}.png`
+}
+})
+
+const goldenEgg = {
+  coordX: 1000,
+  coordY: 500,
+  img: "images/goldenEgg.png"
+}
+
+/*
+
+{
+  minShow,
+  maxShow,
+  minHide,
+  maxHide,
+  coordX,
+  coordY
+}
+
+*/
+
+class Eggs extends Component{
+  render() {
+    return(
+      <div className="Eggs"> 
+        {
+          eggsRandom.map((egg, index) => <Egg key={index} egg={egg} id={index + 1} />)
+        }
+        <GoldenEgg egg={goldenEgg}/>
+      </div>
+    )
+  }
+}
+
+export default Eggs;
