3e600f12e4d113ec765bcc11453717f93f5b4735
Mathias Tujague <mathiastujague@gmail.com>
2019-04-18 18:13:05 +0200

    compteur fonctionnel avec le game over comme voulu

--- src/Credit/Credit.jsx
+++ src/Credit/Credit.jsx
@@ -10,22 +10,35 @@ class Credit extends Component{
     }
 
     getLess = () => {
-        this.setState({credit:this.state.credit - 1})
+        const credit = this.state.credit;
+        if (credit >= 1) {
+            this.setState({credit: credit - 1})
+        }
 
-            if (this.state.credit <= 0) {
-                alert("GAME OVER!!!")  
-                this.setState({credit:10})
-            }
+            // this.state.credit === 0 ? {"GAME OVER"} : {this.state.credit}
+            //  /* if (this.state.credit <= 0) {
+            //     alert("GAME OVER!!!")   
+            //     this.setState({credit:10}) */
+            // }
             
     }
 
     render(){
+        const credit = this.state.credit;
         return(
             <div className="Credit">
+                {
+                    (credit === 0)
+                    ? (<div>Game Over</div>)
+                    : (
+                        <div> 
+                            <h2>Credits: {credit}</h2>
+                            <button onClick={this.getLess}>less</button>
+                        </div>
+                    )
+                }
                 
-                <h2>Credits: {this.state.credit}</h2>
-                <button onClick={this.getLess}>less</button>
-
+                
 
             </div>
 
